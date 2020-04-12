4adc73f06303408ba8eeaab63708757b041abd7e
miguelhun <hunmiguel1124@gmail.com>
2020-04-11 19:35:21 -0500

    Check is blockchain is valid

--- blockchain.py
+++ blockchain.py
@@ -92,6 +92,16 @@ def get_chain():
 
     return jsonify(response), 200
 
+# Check if blockchain is valid
+@app.route('/is_valid', methods=['GET'])
+def get_is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'The blockchain is valid'}
+    else:
+        response = {'message': 'The blockchain is not valid'}
+    return jsonify(response), 200
+
 # Run app
 app.run(host = '0.0.0.0', port = 5000)
 
